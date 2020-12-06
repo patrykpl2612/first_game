9a76114ead226aff6f5567d797452ab3012230ee
Mikrosx <Mikrosx2000@gmail.com>
2020-12-06 20:59:54 +0100

    Update PlayerControls.cs

--- first_game/Assets/PlayerControls.cs
+++ first_game/Assets/PlayerControls.cs
@@ -7,39 +7,34 @@ public class PlayerControls : MonoBehaviour
     public Sprite[] spriteArray;
     public SpriteRenderer spriteRenderer;
     public Animator animator;
+
     public Vector2 speed = new Vector2(50, 50);
     public float fastness;
-
-
-    public int FPS_LIMIT = 30;
-    public int default_animation_speed = 2;
-    public float default_player_speed = 0.1f;
-
 
     void Update()
     {
 
-        int x = 0;      // od -1 do 1
-        int y = 0;      // od -1 do 1
+        int x = 0;
+        int y = 0;
+
 
         bool up = Input.GetButton("Fire1");
         bool down = Input.GetButton("Fire2");
         bool left = Input.GetButton("Fire3");
         bool right = Input.GetButton("Jump");
-        bool space = Input.GetButton("Vertical");
-        bool SwitchAnim = up || down || left || right; //bool determinujacy kiedy zmienic animacje chodzenia
+        bool spaceUp = Input.GetButton("Vertical");
 
-        bool[] inputList = { up, down, left, right, space, SwitchAnim };
-        string[] inputNames = { "Up", "Down", "Left", "Right", "SwitchAnim" };
-        SendBoolInput(inputList, inputNames);
-
-        Debug.Log("Test");
+        animator.SetBool("Right", right);
+        animator.SetBool("Left", left);
+        animator.SetBool("Down", down);
+        animator.SetBool("Up", up);
+        animator.SetBool("SwitchAnim", SwitchAnim(up, down, left, right));
 
         if (up == true && down == false)
         {
             y = 1;
         }
-        else if (down == true && up == false)
+        if (down == true && up == false)
         {
             y = -1;
         }
@@ -48,25 +43,24 @@ public class PlayerControls : MonoBehaviour
         {
             x = -1;
         }
-        else if (right == true && left == false)
+        if (right == true && left == false)
         {
             x = 1;
         }
-
-        if (space)
+        if (x == 0 && y == 0)
         {
-            fastness = default_player_speed * 2;
-            animator.speed = default_animation_speed * 2;
+            animator.speed = 0;
+        }
+
+        if (spaceUp == true)
+        {
+            fastness = 0.2f;
+            animator.speed = 2;
         }
         else
         {
-            fastness = default_player_speed;            // domyslne parametry gracza dotyczace jego predkosci i jego animacji
-            animator.speed = default_animation_speed;
-        }
-
-        if (x == 0 && y == 0)
-        {
-            animator.speed = 0; // Zatrzymaj animacje jesli gracz stoi w miejscu
+            fastness = 0.1f;
+            animator.speed = 1;
         }
 
 
@@ -78,16 +72,10 @@ public class PlayerControls : MonoBehaviour
 
     void Awake()
     {
-        Application.targetFrameRate = FPS_LIMIT;  // Limit klatek !!!WRZUCICĆ TO DO OSOBNEGO PLIKU!!!
+        Application.targetFrameRate = 30;
     }
-
-    public void SendBoolInput(bool[] inputList, string[] inputNames)
+    public bool SwitchAnim(bool a, bool b, bool c, bool d)
     {
-        int i = 0;
-        foreach (string inputName in inputNames)
-        {
-            animator.SetBool(inputName, inputList[i]);  //wysyła stan wartosci do animatora unity (mapowanie kontrolek,animacji)
-            i += 1;
-        }
+        return a || b || c || d;
     }
 }
