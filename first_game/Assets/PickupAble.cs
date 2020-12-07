7654b57ec699730ff19af08b96350865837c3f5a
Igor Baranowski <ipadowski123@gmail.com>
2020-12-07 20:27:00 +0100

    działajonce żucanie i skrypt do żucania
    (y)

--- first_game/Assets/PickupAble.cs
+++ first_game/Assets/PickupAble.cs
@@ -4,86 +4,29 @@ using UnityEngine;
 
 public class PickupAble : MonoBehaviour
 {
-    public bool PickedUp = false;
-    public Transform RockChild;
-    public Transform PlayerParent;
-    public GameObject Player;
-    public Animator PlayerAnimator;
-    public PlayerControls move;
+    public float HeightAbovePlayer = 2.1f;
+    public float ThrowRangeHorizontal = 3f;
+    public float ThrowRangeVertical = 3f;
 
-    void Start()
+    public float Get(string what)
     {
-        RockChild = transform.Find("/InterRock");
-        PlayerParent = transform.Find("/Player");
-        Player = GameObject.Find("/Player");
-        move = Player.GetComponent<PlayerControls>();
-        PlayerAnimator = Player.GetComponent<Animator>();
-    }
-
-    void OnCollisionEnter2D(Collision2D Object)
-    {
-        var obiekt = Object.collider;
-
-        Vector3 playerPosition = GameObject.Find("Player").transform.position;
-
-        Vector3 rockPosition = playerPosition;
-        if (obiekt.transform.position == GameObject.Find("/Player").transform.position)
+        if (what == "Height")
         {
-            PickedUp = true;
-            rockPosition[1] += 2.1f;
-            RockChild.transform.SetParent(PlayerParent, true);
-            RockChild.transform.Translate(rockPosition - RockChild.position);
+            return HeightAbovePlayer;
         }
-
-
-    }
-    void FixedUpdate()
-    {
-
-        bool rzut = Input.GetButton("PutDown");
-        int x = move.x;
-        int y = move.y;
-       // Vector3 changedscale = this.transform.localScale;
-        //changedscale[0] *= -1;
-
-      /*  if (x == 1)
+        if (what == "RangeH")
         {
-            this.transform.localScale = changedscale;
+            return ThrowRangeHorizontal;
         }
-        if(x==-1)
+        if (what == "RangeV")
         {
-            this.transform.localScale = changedscale;
-        }*/
-
-        if (PickedUp && rzut && y == 0 && x == 0)
+            return ThrowRangeVertical;
+        }
+        else
         {
-
-
-            Vector3 playerPosition = GameObject.Find("Player").transform.position;
-            Vector3 rockPosition = playerPosition;
-            if (PlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("PlayerStandingLeft"))
-            {
-                rockPosition[0] -= 3f;
-            }
-            if (PlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("PlayerStandingRight"))
-            {
-
-                rockPosition[0] += 3f;
-            }
-            if (PlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("PlayerStandingUp"))
-            {
-                rockPosition[1] += 3f;
-            }
-            if (PlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("PlayerStandingDown"))
-            {
-                rockPosition[1] -= 3f;
-            }
-           
-            RockChild.transform.Translate(rockPosition - RockChild.position);
-            RockChild.transform.SetParent(null);
-            PickedUp = false;
-            
+            return 0f;
         }
 
     }
+    
 }
--- first_game/Assets/PlayerControls.cs
+++ first_game/Assets/PlayerControls.cs
@@ -7,12 +7,17 @@ public class PlayerControls : MonoBehaviour
     public Sprite[] spriteArray;
     public SpriteRenderer spriteRenderer;
     public Animator animator;
-    public Vector2 speed = new Vector2(50, 50);
-    public float fastness;
-    public bool Collides=false;
-    public int x, y;
 
- 
+    public GameObject HeldItem;
+    public Vector2 speed = new Vector2(50, 50);
+    public bool Holding = false;
+    public float fastness;
+    public bool Collided=false;
+    public int x, y;
+    public string Facing = null;
+
+
+
     public int default_animation_speed = 2;
     public float default_player_speed = 0.1f;
 
@@ -28,9 +33,11 @@ public class PlayerControls : MonoBehaviour
         bool left = Input.GetButton("Left");
         bool right = Input.GetButton("Right");
         bool space = Input.GetButton("Run");
-        
+        bool putDown = Input.GetButton("PutDown");
 
-        bool[] inputList = { up, down, left, right, space };
+
+
+        bool[] inputList = { up, down, left, right,};       //listy do przeslania booli do animatora
         string[] inputNames = { "Up", "Down", "Left", "Right"};
         SendBoolInput(inputList, inputNames);
 
@@ -39,19 +46,23 @@ public class PlayerControls : MonoBehaviour
         if (up == true && down == false)
         {
             y = 1;
+            Facing = "N";
         }
         else if (down == true && up == false)
         {
             y = -1;
+            Facing = "S";
         }
 
         if (left == true && right == false)
         {
             x = -1;
+            Facing = "W";
         }
         else if (right == true && left == false)
         {
             x = 1;
+            Facing = "E";
         }
 
         if (space) 
@@ -65,20 +76,91 @@ public class PlayerControls : MonoBehaviour
             animator.speed = default_animation_speed;
         }
 
-        if (Collides == false)
+        if (Collided == false)
         {
             Vector3 movement = new Vector3(speed.x * x * fastness, speed.y * y * fastness, 0);
 
             movement *= Time.fixedDeltaTime;
             transform.Translate(movement);
         }
-        Collides = false;
+
+        if (Holding)
+        {
+            Pickup(HeldItem);
+        }
+        if (Holding && putDown && x == 0 && y == 0)
+        {
+            Throw(HeldItem);
+        }
+
+        Collided = false;
 
     }
     void OnCollisionEnter2D(Collision2D Object)
     {
-        Collides = true;
+        Collided = true;
+
+        GameObject CollidedObject = GameObject.Find(Object.collider.name);
+        if (CollidedObject.GetComponent<PickupAble>() != null && Holding == false) // sprawdz czy obiekt mozna podniesc
+        {
+            Holding = true;
+            HeldItem = CollidedObject;          
+        }     
     }
+
+    void Pickup(GameObject Object)
+    {       
+        Object.transform.SetParent(transform, true);              
+        Object.transform.Translate((transform.position) - Object.transform.position);
+        Object.transform.Translate(0f, Object.GetComponent<PickupAble>().Get("Height"), 0f);
+        
+        Collider2D m_Collider = Object.GetComponent<Collider2D>();
+        SpriteRenderer m_Renderer = Object.GetComponent<SpriteRenderer>();
+
+
+        if (m_Collider.enabled)
+        {
+            m_Collider.enabled = false;
+            m_Renderer.sortingOrder = 3;
+        }
+    }
+
+    void Throw(GameObject Object)
+    {
+        Collider2D m_Collider = Object.GetComponent<Collider2D>();
+        SpriteRenderer m_Renderer = Object.GetComponent<SpriteRenderer>();
+
+        m_Renderer.sortingOrder = 1;
+        m_Collider.enabled = true;
+        Holding = false;
+        HeldItem = null;
+        float head = Object.GetComponent<PickupAble>().Get("Height");
+        float rangeH = Object.GetComponent<PickupAble>().Get("RangeH");
+        float rangeV = Object.GetComponent<PickupAble>().Get("RangeV");
+
+
+
+        if (Facing == "W")
+        {
+            Object.transform.Translate(rangeH * -1f, 0f -head, 0f);  //rzut w lewo
+        }   
+        if (Facing == "E")
+        {
+            Object.transform.Translate(rangeH, 0f - head, 0f);   //rzut w prawo
+        }
+        if (Facing == "N")
+        {
+            Object.transform.Translate(0f, rangeV - head, 0f); //rzut w górę
+        }
+        if (Facing == "S")
+        {
+            Object.transform.Translate(0f, (rangeV * -1f) - head, 0f); //rzut w dół
+        }
+      
+        Object.transform.SetParent(null);
+    }
+
+
 
 
     public void SendBoolInput(bool[] inputList, string[] inputNames)
@@ -90,4 +172,5 @@ public class PlayerControls : MonoBehaviour
             i += 1;
         }
     }
+
 }
