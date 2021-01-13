f11d4da0a04278c7fab35ff70abd06c28ec65714
patryk001001 <patryk0076@gmail.com>
2021-01-13 20:28:51 +0100

    Sklep działa

--- first_game/Assets/Scripts/NPC/NPCDealer.cs
+++ first_game/Assets/Scripts/NPC/NPCDealer.cs
@@ -4,30 +4,72 @@ using UnityEngine;
 
 public class NPCDealer : MonoBehaviour
 {
+    [SerializeField] private UI_Inventory uiInventory;
+
     public float smooth = 1f;
     public Animator animator;
     private Quaternion targetRotation;
+    private Inventory inventory;
+    public GameObject inventoryObject;
 
     public int x = 1;
     // Start is called before the first frame update
 
 
     public GameObject ThePlayer;
+    public PlayerControls player;
     public float Radius;
+    public Transform target;
 
 
     void Start()
     {
-        
+
+    }
+
+    private int GetPrice(string itemType)
+    {
+        switch (itemType)
+        {
+            default:
+            case "HealthPotion":    return 1;
+            case "Coin":            return 2;
+            case "Muszla1":         return 3;
+            case "Muszla2":         return 4;
+            case "Muszla3":         return 5;
+            case "Muszla4":         return 6;
+            case "Muszla5":         return 7;
+        }
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(2);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        x *= -1;
-      
-        Vector3 newScale = transform.localScale;
-        newScale.x *= -1;
-        transform.localScale = newScale;
+        
+        
+        ItemWorld itemWorld = collider.GetComponent<ItemWorld>();
+        if (itemWorld != null)
+        {
+            StartCoroutine(ExampleCoroutine());
+            int priceForItem = GetPrice(itemWorld.GetItem().itemType.ToString());
+            int amount = itemWorld.GetItem().amount;
+
+            Item duplicateItem = new Item { itemType = Item.ItemType.Coin, amount = priceForItem * amount };
+            //transform.LookAt(target);
+            ItemWorld.DropItem(this.transform.position, "S", duplicateItem);
+            itemWorld.DestroySelf();
+        }
+        else
+        {
+            x *= -1;
+            Vector3 newScale = transform.localScale;
+            newScale.x *= -1;
+            transform.localScale = newScale;
+        }
     }
 
     // Update is called once per frame
