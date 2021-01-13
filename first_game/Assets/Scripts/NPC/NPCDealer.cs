fd0c93352e9b4a61f6e6cb2b0a5a3014548284da
patryk001001 <patryk0076@gmail.com>
2021-01-13 15:50:58 +0100

    BiEgA jAk PoYeBaNy

---
+++ first_game/Assets/Scripts/NPC/NPCDealer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDealer : MonoBehaviour
+{
+    public float smooth = 1f;
+
+    private Quaternion targetRotation;
+
+    public int x = 1;
+    // Start is called before the first frame update
+
+
+    public GameObject ThePlayer;
+    public float Radius;
+
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        x *= -1;
+      
+        Vector3 newScale = transform.localScale;
+        newScale.x *= -1;
+        transform.localScale = newScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(ThePlayer.transform.position, transform.position);
+        
+        if (dist > Radius)
+        {
+            Vector3 movement = new Vector3(x, 0, 0);
+            movement *= Time.fixedDeltaTime;
+            transform.Translate(movement);
+        }
+
+
+        
+        
+
+    }
+}
