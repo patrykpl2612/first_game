4b5df3bde0fd0e0c9817e68b7eab727183164c3c
patryk001001 <patryk0076@gmail.com>
2021-01-14 17:00:05 +0100

    Handlarz npc wyrzuca itemy i jest gicior

--- first_game/Assets/Scripts/NPC/NPCDealer.cs
+++ first_game/Assets/Scripts/NPC/NPCDealer.cs
@@ -20,7 +20,7 @@ public class NPCDealer : MonoBehaviour
     public PlayerControls player;
     public float Radius;
     public Transform target;
-
+    private ItemWorld itemWorld;
 
     void Start()
     {
@@ -33,7 +33,7 @@ public class NPCDealer : MonoBehaviour
         {
             default:
             case "HealthPotion":    return 1;
-            case "Coin":            return 2;
+            case "Coin":            return 1;
             case "Muszla1":         return 3;
             case "Muszla2":         return 4;
             case "Muszla3":         return 5;
@@ -42,26 +42,32 @@ public class NPCDealer : MonoBehaviour
         }
     }
 
-    IEnumerator ExampleCoroutine()
+    IEnumerator takeItem()
     {
-        yield return new WaitForSeconds(2);
+        int priceForItem = GetPrice(itemWorld.GetItem().itemType.ToString());
+        int amount = itemWorld.GetItem().amount;
+        Item duplicateItem = new Item { itemType = Item.ItemType.Coin, amount = priceForItem * amount };
+        animator.SetBool("walk", false);
+        yield return new WaitForSeconds(0.4f);
+        animator.SetBool("jump", true);
+        yield return new WaitForSeconds(1);
+        animator.SetBool("jump", false);
+        yield return new WaitForSeconds(1);
+        ItemWorld.DropItem(this.transform.position, "S", duplicateItem);
+        itemWorld.DestroySelf();
+        yield return new WaitForSeconds(1);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         
         
-        ItemWorld itemWorld = collider.GetComponent<ItemWorld>();
+        itemWorld = collider.GetComponent<ItemWorld>();
         if (itemWorld != null)
         {
-            StartCoroutine(ExampleCoroutine());
-            int priceForItem = GetPrice(itemWorld.GetItem().itemType.ToString());
-            int amount = itemWorld.GetItem().amount;
-
-            Item duplicateItem = new Item { itemType = Item.ItemType.Coin, amount = priceForItem * amount };
-            //transform.LookAt(target);
-            ItemWorld.DropItem(this.transform.position, "S", duplicateItem);
-            itemWorld.DestroySelf();
+            
+            StartCoroutine(takeItem());
+            
         }
         else
         {
@@ -76,18 +82,18 @@ public class NPCDealer : MonoBehaviour
     void Update()
     {
         float dist = Vector3.Distance(ThePlayer.transform.position, transform.position);
-        
+
 
         if (dist > Radius)
         {
-            animator.SetBool("walk", false);
-           
+            animator.SetBool("idle", false);
+            animator.SetBool("walk", true);
             Vector3 movement = new Vector3(x, 0, 0);
             movement *= Time.fixedDeltaTime;
             transform.Translate(movement);
-            
+
         }
-        else animator.SetBool("walk", true);
+        else { animator.SetBool("walk", false); animator.SetBool("idle", true); }
 
 
 
